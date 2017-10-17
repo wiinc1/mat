0720a1e558de49a122cd96a597281be25eff6e98
Brian Wuollet <bwuollet@is-mbp-bwuollet3.in.ionicsecurity.com>
2017-10-17 18:36:55 -0400

    Implemented authcontrolpolicy to validate registration info meets spec

--- server/src/routes.js
+++ server/src/routes.js
@@ -1,7 +1,8 @@
+const AuthenticationController = require('./controllers/AuthenticationController')
+const AuthenticationControllerPolicy = require('./policies/AuthenticationControllerPolicy')
+
 module.exports = (app) => {
-  app.post('/register', (req, res) => {
-    res.send({
-      message: `Hello ${req.body.email}! Your user was registered!`
-    })
-  })
+  app.post('/register',
+    AuthenticationControllerPolicy.register,
+    AuthenticationController.register)
 }
