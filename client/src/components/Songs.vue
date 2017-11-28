b83624b4f5ac9289e6b607b718ac8e0096ddff57
Brian Wuollet <bwuollet@is-mbp-bwuollet3.in.ionicsecurity.com>
2017-11-28 08:46:47 -0500

    Imp song endpoint,

--- client/src/components/Songs.vue
+++ client/src/components/Songs.vue
@@ -2,17 +2,31 @@
   <v-layout column>
     <v-flex xs6 offset-xs3>
       <panel title="Songs">
-
+        <div v-for="song in songs"
+          :key="song.id">
+          {{song.title}}
+          {{song.artist}}
+          {{song.album}}
+        </div>
       </panel>
     </v-flex>
   </v-layout>
 </template>
 
 <script>
+import SongService from '@/services/SongService'
 import Panel from '@/components/Panel'
 export default {
   components: {
     Panel
+  },
+  data () {
+    return {
+      songs: null
+    }
+  },
+  async mounted () {
+    this.songs = (await SongService.index()).data
   }
 }
 </script>
